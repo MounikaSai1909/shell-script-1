9e4946582fd1380a50fc957c43af2a7ce019c61b
putti swamy <puttiswamy123@gmail.com>
2024-05-04 23:03:19 +0530

    Sampe file

---
+++ arrays.sh
@@ -0,0 +1,11 @@
+
+MOVIES=("Arya" "Desamuduru" "Pushpa")
+
+#list always starts with 0 (index always with 0)
+# size of above array is 3
+
+
+echo " Firt movie is ${MOVIES[0]}"
+echo " second movie is $MOVIES[1]}"
+
+echo " All movies $MOVIES[@]}"
