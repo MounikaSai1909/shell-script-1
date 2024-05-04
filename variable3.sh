a3d7ddd5e036fda121bde6b61ae515bbe723d704
putti swamy <puttiswamy123@gmail.com>
2024-05-04 22:32:53 +0530

    Sampe file

---
+++ variable3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#declaring the variable
+PERSON1==$1; # no space between = and value
+PERSON2=$2;
+
+
+#referring of variable
+echo " $PERSON1 :: Hello $PERSON2,How are you "
+echo " $PERSON2 :: Hi $PERSON1 ,I am fine. How are you "
+echo " $PERSON1 :: I am fine too,How is your work "
+echo " $PERSON2 :: not bad .I am thinking to learn new skills"
