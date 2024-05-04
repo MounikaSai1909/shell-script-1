59c90f66f70437b18d84d861af84d96211ff9bff
putti swamy <puttiswamy123@gmail.com>
2024-05-04 23:32:49 +0530

    Sampe file

---
+++ Special-variables-5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variables : $@ "
+echo "Number of varaibles : $# "
+echo "Script name : $0 "
+echo "current working directory : $PWD "
+echo "home directory of current user : $HOME "
+echo "which user is running script : $USER "
+echo "Hostname : $HOSTNAME "
+echo "process id of current shell script : $$ "
+echo "process if of last background command : $! "
