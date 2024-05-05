6cb2511e978f104e27cf3c55dc91d14e915f7984
putti swamy <puttiswamy123@gmail.com>
2024-05-05 17:58:20 +0530

    Sample commit

--- installation.sh
+++ installation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-dnf install mysql -y
+
+USERID=$(id -u)
+
+if [ $uSERID -eq 0 ]
+then 
+   echo " please proceed with installation "
+   dnf install mysql -y
+else 
+    echo " please run the script with root access"
+    exit 1 #  manually exit if error comes
+fi
