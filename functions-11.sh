3a3bffe21f20c509eb64a0ebd1e1f384876efb5c
putti swamy <puttiswamy123@gmail.com>
2024-05-21 17:54:52 +0530

    function commit

--- functions-11.sh
+++ functions-11.sh
@@ -2,13 +2,17 @@
 #USERID=$(id -u)
 
 VALIDATE (){
-   if [ $1  -ne 0 ]
-    then
-       echo "$2... FAILURE "
-       exit 1
-    else
-         echo "$2.. SUCCESS"
-    fi  
+ #  if [ $1  -ne 0 ]
+ #  then
+ #    echo "$2... FAILURE "
+ #      exit 1
+ #   else
+ #        echo "$2.. SUCCESS"
+ #   fi  
+    echo " 1st argument : $1"
+    echo " 2nd argument : $2"
+    echo " 3rd argument : $3"
+
 }
 
 
@@ -18,3 +22,6 @@ VALIDATE $? "installing MySQL"
 
 dnf install git -y
 VALIDATE $? "installing Git"
+
+dnf install nodejs -y
+VALIDATE $? "installing nodejs"
