9db3e2854ac3ec515880a8a32e3f543339323c2d
putti swamy <puttiswamy123@gmail.com>
2024-05-21 18:42:07 +0530

    function commit

--- functions-11.sh
+++ functions-11.sh
@@ -1,8 +1,11 @@
 
 #USERID=$(id -u)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+echo " SCRIPT_NAME : $SCRIPT_NAME "
 TIMESTAMP=$(date +%F-%H-%M-%S)
+echo " TIMESTAMP : $TIMESTAMP "
 LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+echo " LOG_FILE : $LOG_FILE "
 
 VALIDATE (){
    if [ $1  -ne 0 ]
