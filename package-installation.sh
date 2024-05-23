75cf571fa10fa07614cabb52e6c536f63049b680
putti swamy <puttiswamy123@gmail.com>
2024-05-23 19:07:23 +0530

    sample file

--- package-installation.sh
+++ package-installation.sh
@@ -4,10 +4,16 @@
 # colours are mandatory
 # user have root access or not
 # send packages to install as paramters
+#  yum list installed mysql -- to check the list of package installed in server
 
 #sudo sh package-installation.sh mysql git gcc docker
 
 USERID=$(id -u)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+
 if [ $USERID -ne 0 ]
 then 
     echo " Please run with super user "
@@ -17,5 +23,12 @@ fi
 
 for i in $@
 do
-echo " package to install : $i " 
+   echo " package to install : $i " 
+   yum list installed $i &>> $LOG_FILE
+   if [ $? -eq 0 ]
+   then 
+     echo "$i already installed... SKIPPING"
+    else
+     echo "$i not installed... INSTALL"
+    fi
 done
