48c954f8c4bb6c77eef1c5a3fa8524de7603dc4d
putti swamy <puttiswamy123@gmail.com>
2024-06-05 20:10:03 +0530

    sample file

---
+++ delete-old-files.sh
@@ -0,0 +1,23 @@
+# delete old files more than 2 weeks old
+
+# 1.decide which folder,check the folder exists or not
+# 2. dont delete files ending with .java,.sh, .py ,delete only ending with .log 
+# 3. find and delete the files which are more than 2 weeks old
+
+
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+SOURCE_DIRECTORY=/tmp/app-logs
+
+if [ -d $SOURCE_DIRECTORY ]
+then
+   echo -e "$G $SOURCE_DIRECTORY exists $N"
+else
+   echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
+   exit 1
+fi 
+##
